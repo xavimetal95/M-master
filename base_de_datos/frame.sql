14edae586bbd6dce4824c849dfdbb2bcc1bb0160
xavimetal95 <alumnes@inf1.cesnuria>
2016-06-17 16:53:59 +0200

    base

--- base_de_datos/frame.sql
+++ base_de_datos/frame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2016 a las 18:10:17
+-- Tiempo de generación: 17-06-2016 a las 16:42:31
 -- Versión del servidor: 5.6.26
 -- Versión de PHP: 5.6.12
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `mydb`
+-- Base de datos: `frame`
 --
 
 DELIMITER $$
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `anuncio` (
   `descripcion` varchar(1024) COLLATE utf8_unicode_ci DEFAULT NULL,
   `longitud` int(11) NOT NULL,
   `latitud` int(11) NOT NULL,
-  `Usuarios_id_user` int(11) NOT NULL DEFAULT '0',
+  `Usuarios_id_user` int(11) NOT NULL,
   `Usuarios_id_user1` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -56,32 +56,7 @@ CREATE TABLE IF NOT EXISTS `anuncio` (
 --
 
 INSERT INTO `anuncio` (`id_anuncio`, `imagen`, `titulo`, `descripcion`, `longitud`, `latitud`, `Usuarios_id_user`, `Usuarios_id_user1`) VALUES
-(1, 'url', 'titulo tituloso', 'Se vende el mejor coche del planeta tierra porque lo digo yo, Joseba de carglas :D', 123, 123, 0, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `juegos`
---
-
-CREATE TABLE IF NOT EXISTS `juegos` (
-  `idjuegos` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `descripcion` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `niveles`
---
-
-CREATE TABLE IF NOT EXISTS `niveles` (
-  `idniveles` int(11) NOT NULL,
-  `nivel` varchar(45) NOT NULL,
-  `puntuacion` float DEFAULT NULL,
-  `juegos_idjuegos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(1, 'https://www.repsol.com/creatividad/productos_y_servicios/lubricantes/gama_moto/es_es/img/Moto2.png', 'moto', 'moto chula', 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -127,7 +102,7 @@ INSERT INTO `usuarios` (`id_user`, `email`, `nombre`, `pass`, `rol`) VALUES
 (1, 'prueba@gmail.com', 'prueba', '1234', 'admin'),
 (2, 'prueba2@gmail.com', 'prueba2', '1234', 'client'),
 (6, 'pepe2', 'antonio', '1234', 'user'),
-(7, 'pepepe', 'pepe2', '1234', 'client'),
+(7, 'pepe@gmail.com', 'pepito', '1234', 'client'),
 (8, 'entoÃ±ico @gmail.com', 'antonio josÃ©', '1234', 'cliente');
 
 -- --------------------------------------------------------
@@ -153,19 +128,6 @@ CREATE TABLE IF NOT EXISTS `valoracion` (
 ALTER TABLE `anuncio`
   ADD PRIMARY KEY (`id_anuncio`,`Usuarios_id_user`),
   ADD KEY `fk_Anuncio_Usuarios1_idx` (`Usuarios_id_user1`);
-
---
--- Indices de la tabla `juegos`
---
-ALTER TABLE `juegos`
-  ADD PRIMARY KEY (`idjuegos`);
-
---
--- Indices de la tabla `niveles`
---
-ALTER TABLE `niveles`
-  ADD PRIMARY KEY (`idniveles`),
-  ADD KEY `fk_niveles_juegos1_idx` (`juegos_idjuegos`);
 
 --
 -- Indices de la tabla `poblacion`
@@ -203,16 +165,6 @@ ALTER TABLE `valoracion`
 ALTER TABLE `anuncio`
   MODIFY `id_anuncio` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT de la tabla `juegos`
---
-ALTER TABLE `juegos`
-  MODIFY `idjuegos` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `niveles`
---
-ALTER TABLE `niveles`
-  MODIFY `idniveles` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT de la tabla `poblacion`
 --
 ALTER TABLE `poblacion`
@@ -241,12 +193,6 @@ ALTER TABLE `valoracion`
 --
 ALTER TABLE `anuncio`
   ADD CONSTRAINT `fk_Anuncio_Usuarios1` FOREIGN KEY (`Usuarios_id_user1`) REFERENCES `usuarios` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `niveles`
---
-ALTER TABLE `niveles`
-  ADD CONSTRAINT `fk_niveles_juegos1` FOREIGN KEY (`juegos_idjuegos`) REFERENCES `juegos` (`idjuegos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `valoracion`
